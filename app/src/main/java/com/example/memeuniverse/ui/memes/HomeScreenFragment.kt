86ff738ca756dc8ca91d0503e8334961e43d801f
Aman saxena <asaxena7531@gmail.com>
2021-11-27 13:45:48 +0530

    showing memes in ui

--- app/src/main/java/com/example/memeuniverse/ui/memes/HomeScreenFragment.kt
+++ app/src/main/java/com/example/memeuniverse/ui/memes/HomeScreenFragment.kt
@@ -7,14 +7,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.LifecycleObserver
+import com.bumptech.glide.Glide
 import com.example.memeuniverse.R
 import com.example.memeuniverse.ui.memes.viewmodels.MemeViewModel
 
 
 class HomeScreenFragment : Fragment() {
+    private lateinit var memeImgView:ImageView
+    private lateinit var memetv:TextView
+    private lateinit var memeauthortv:TextView
     private lateinit var nextBtn: Button
     private val model:MemeViewModel by activityViewModels()
 
@@ -30,12 +36,17 @@ class HomeScreenFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home_screen, container, false)
         nextBtn = view.findViewById(R.id.fetchmemesbtn)
+        memeImgView = view.findViewById(R.id.memeImageView)
+        memeauthortv = view.findViewById(R.id.memeAuthorTextView)
+        memetv = view.findViewById(R.id.memeTitleTextView)
         model.fetchMemes().observe(viewLifecycleOwner,{
             Log.d("memes", "onResponse: $it")
+            memeauthortv.text = "Author : " + it.author
+            memetv.text = "Title : "+it.title
+            Glide.with(requireContext()).load(it.url).into(memeImgView)
         })
         nextBtn.setOnClickListener {
             model.fetchMemes()
-            Toast.makeText(requireContext(),"Clicked",Toast.LENGTH_SHORT).show()
         }
         return view
     }
